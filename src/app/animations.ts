a23fc8c56f1a2ed89ebcdf8f0031d6061c48281e
Casey <caseybermanmusic@gmail.com>
2019-06-06 13:39:04 -0400

    ended up making some router animation fixes first

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -11,122 +11,104 @@ import {
 } from "@angular/animations";
 
 export const slideInAnimation = trigger("routeAnimations", [
-  transition("HomePage => *", [
-    style({ position: "relative" }),
-    query(":enter, :leave", [
-      style({
-        position: "absolute",
-        top: 0,
-        left: 0,
+  // transition("HomePage => *", [
+  //   style({ position: "relative" }),
+  //   query(":enter, :leave", [
+  //     style({
+  //       position: "absolute",
+  //       //   top: 0,
+  //       //   left: 0,
+  //       width: "100%",
+  //     }),
+  //   ]),
+  //   query(
+  //     ":enter",
+  //     [
+  //       style({
+  //         opacity: 0,
+  //       }),
+  //       animate("800ms", style({ left: 1 })),
+  //     ],
+  //     {
+  //       optional: true,
+  //     }
+  //   ),
 
-        width: "100%",
-      }),
-    ]),
-    query(
-      ":enter",
-      [
+  //   // query("@gigBallIn", animateChild(), { optional: true }),
+  //   // query(":leave", animateChild(), { optional: true }),
+  //   group([
+  //     // works but simplifying for now
+  //     query(":leave", [animate("800ms ease-in-out", style({ opacity: 0 }))], {
+  //       optional: true,
+  //     }),
+  //     query(
+  //       ":enter",
+  //       [animate("800ms cubic-bezier(.9,.3,.8,.3)", style({ opacity: "1" }))],
+  //       { optional: true }
+  //     ),
+  //   ]),
+  //   // query("@gigBallIn", animateChild()),
+  // ]),
+
+  // need to write:
+  // transition("* => HomePage", []),
+
+  // transition("MusicPage => GigsPage, ContactPage => GigsPage", [
+
+  // all pages where we shift rightwards
+  transition(
+    "ContactPage => MusicPage, ContactPage => GigsPage, GigsPage => MusicPage",
+    [
+      style({ position: "relative" }),
+      query(":enter, :leave", [
         style({
-          left: "-100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
         }),
-        animate("800ms", style({ left: 0 })),
-      ],
-      {
-        optional: true,
-      }
-    ),
-    query(":leave", animateChild(), { optional: true }),
-    group([
-      // works but simplifying for now
-      query(
-        ":leave",
-        [
-          // animate("700ms ease-out", style({ left: "100%", opacity: 0 })),
-          animate("800ms ease-in-out", style({ opacity: 0 })),
-        ],
-        { optional: true }
-      ),
-      query(
-        ":enter",
-        [
-          animate(
-            "800ms 1500ms cubic-bezier(.9,.3,.8,.3)",
-            style({ opacity: "1" })
-          ),
-        ],
-        { optional: true }
-      ),
-    ]),
-    // query("@gigBallIn", animateChild()),
-  ]),
-  transition("* => HomePage", []),
-  transition("MusicPage => GigsPage, ContactPage => GigsPage", [
-    style({ position: "relative" }),
-    query(":enter, :leave", [
-      style({
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-      }),
-    ]),
-    query(":enter", [style({ top: "100vh" })], { optional: true }),
-    query(":leave", animateChild(), { optional: true }),
-    group([
-      query(":leave", [animate("700ms ease-out", style({ opacity: 0 }))], {
-        optional: true,
-      }),
-      query(
-        ":enter",
-        [animate("800ms ease-out", style({ top: "0vh", left: "0" }))],
-        { optional: true }
-      ),
-    ]),
-    query("@gigBallIn", animateChild()),
-  ]),
-  transition("GigsPage => MusicPage, ContactPage => MusicPage", [
-    style({ position: "relative" }),
-    query(":enter, :leave", [
-      style({
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-      }),
-    ]),
-    query(":enter", [style({ left: "-100%" })], { optional: true }),
-    query(":leave", animateChild(), { optional: true }),
-    group([
-      query(":leave", [animate("700ms ease-out", style({ left: "100%" }))], {
-        optional: true,
-      }),
-      query(":enter", [animate("800ms ease-out", style({ left: "0%" }))], {
-        optional: true,
-      }),
-    ]),
-    query(":enter", animateChild()),
-  ]),
-  transition("* => ContactPage", [
-    style({ position: "relative" }),
-    query(":enter, :leave", [
-      style({
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-      }),
-    ]),
-    query(":enter", [style({ left: "-100%" })], {
-      optional: true,
-    }),
-    query(":leave", animateChild(), { optional: true }),
-    group([
-      query(":leave", [animate("700ms ease-out", style({ left: "100%" }))], {
-        optional: true,
-      }),
-      query(":enter", [animate("800ms ease-out", style({ left: "0%" }))], {
-        optional: true,
-      }),
-    ]),
-    query(":enter", animateChild(), { optional: true }),
-  ]),
+      ]),
+      query(":enter", [style({ left: "-100%" })], { optional: true }),
+      query(":leave", [style({left: "0%"})]
+      animateChild(), { optional: true }),
+      group([
+        query(":leave", [animate("700ms ease-out", style({ opacity: 0 }))], {
+          optional: true,
+        }),
+        query(
+          ":enter",
+          [animate("800ms ease-out", style({ top: "0vh", left: "0" }))],
+          { optional: true }
+        ),
+      ]),
+      query("@gigBallIn", animateChild(), {optional: true}),
+    ]
+  ),
+  // all pages where we shift leftwards
+  transition(
+    "MusicPage => GigsPage, GigsPage => ContactPage, MusicPage => ContactPage",
+    [
+      style({ position: "relative" }),
+      query(":enter, :leave", [
+        style({
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+        }),
+      ]),
+      query(":enter", [style({ left: "100%" })], { optional: true }),
+      query(":leave", animateChild(), { optional: true }),
+      group([
+        query(":leave", [animate("700ms ease-out", style({ left: "-100%" }))], {
+          optional: true,
+        }),
+        query(":enter", [animate("800ms ease-out", style({ left: "0%" }))], {
+          optional: true,
+        }),
+      ]),
+      query(":enter", animateChild()),
+      query("@gigBallIn", animateChild(), {optional: true}),
+    ]
+  ),
 ]);
